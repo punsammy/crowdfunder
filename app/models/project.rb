c57b20ca672b8ad71b74df304f4379e161ca29d6
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-08 14:00:02 -0400

    add uniquness for project title

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,8 @@ class Project < ActiveRecord::Base
   has_many :pledges, through: :rewards
   has_many :backers, through: :rewards
   accepts_nested_attributes_for :rewards
-  validates_presence_of :title, :description, :goal, :start_date, :end_date
+  validates :title, uniqueness: true
+  validates_presence_of :description, :goal, :start_date, :end_date
   validates :goal, numericality: { greater_than: 0 }
 
   paginates_per 5
